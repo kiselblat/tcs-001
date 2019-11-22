b9c4fa988f2fc1804b39706a49b1f19ab0db21c9
Tom <tsechrist@outlook.com>
2019-11-21 22:21:52 -0600

    Retreives the necessary info. Minimum requirements met

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,49 @@
-var db = require('../models');
+const axios = require('axios');
+
+const db = require('../models');
 
 module.exports = (app) => {
+
   app.get('/products/:id', (request, response) => {
-    response.json(request.params.id);
 
-    // TODO: GET request to external API
+    let replyObject = {};
+    let url = `https://redsky.target.com/v2/pdp/tcin/${request.params.id}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics`
 
-    // TODO: find record in local noSQL database
+    replyObject.id = request.params.id;
+    
+    // GET request to external API
+    axios.get(url).then(getReply => {
+      
+      replyObject.name = getReply.data.product.item.product_description.title;
+      
+      // find record in local noSQL database
+      db.myRetailItem.find({ myRetailId: request.params.id })
+      .then(dbmyRetailItem => {
+        // first check if item exists in local price database
+        if (dbmyRetailItem.length) {
+          replyObject.current_price = {
+            value: dbmyRetailItem[0].price,
+            currency_code: dbmyRetailItem[0].currency
+          };
+          response.json(replyObject);
+        // if no local record exists, create one
+        } else {
+          db.myRetailItem.create({ myRetailId: request.params.id })
+          .then(dbmyRetailItem => {
+            replyObject.current_price = {
+              value: dbmyRetailItem.price,
+              currency_code: dbmyRetailItem.currency
+            };
+            response.json(replyObject);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        }
+      });
 
-    // TODO: Return json object
+    })
+
   });
+
 }
